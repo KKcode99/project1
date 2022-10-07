c28dd6acc2eeb55cd5a0bb524a6c83b421482e92
Karen <example@gmail.com>
2022-10-07 12:53:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// // const result = confirm('Are you here?');
+
+// // const answer = prompt("Да?", "");
+// // console.log(answer);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+
+
+// const category = "toys";
+
+// console.log(`http://vk.com/${category}/5`);
+
+// const user = 'Karen';
+
+// alert(`Hello, are you ${user}?`)
+
+
